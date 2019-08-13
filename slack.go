27f2ab2da06ea44101f79301b02695c978c2c4be
michaelt <michaelt@blend.com>
2019-08-13 15:16:38 -0400

    slack package

---
+++ slack.go
@@ -0,0 +1,26 @@
+package slack
+
+import (
+	"io/ioutil"
+	"net/http"
+)
+
+// Slack manages slack work
+type Slack struct {
+	RequestSigningSecret []byte
+}
+
+// VerifyRequest verifies the request and returns the posted data
+func (s *Slack) VerifyRequest(r *http.Request) (*SlashCommandRequest, error) {
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		return nil, err
+	}
+	timestamp := r.Header.Get(TimestampHeaderParam)
+	sig := r.Header.Get(SignatureHeaderParam)
+	err = VerifyRequest(timestamp, string(body), sig, s.RequestSigningSecret)
+	if err != nil {
+		return nil, err
+	}
+	return UnmarshalSlashCommandBody(body)
+}
